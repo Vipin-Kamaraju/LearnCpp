8d837918b5b3f4a28b29190b8f0aaa4373ec8754
Kamaraju Vipin (CC-DA/EYC5) <kav8rt@bosch.com>
2020-02-06 18:58:01 +0100

    Perm Missing Element 50%

---
+++ src/CodilityLessons/03_TimeComplexity/01_PermMissingElem.cpp
@@ -0,0 +1,72 @@
+
+/*
+
+Codility Tasks:
+
+
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+A[0] = 2
+A[1] = 3
+A[2] = 1
+A[3] = 5
+
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int PermMissingElem(vector<int> &A)
+{
+	sort(A.begin(), A.end());
+	int numberOfEltsA = A.size();
+
+	int count = 1;
+
+	for (vector<int>::iterator i = A.begin(); i != A.end(); i++)
+	{
+		if (count != *i)
+		{
+			return count;
+		}
+			
+		count++;
+	}
+
+	
+	return 0;
+}
+
+void main()
+{
+	vector<int> A;
+	A = { 2,3,1,5 };
+	int result;
+	result = PermMissingElem(A);
+
+	cout << "missing element = " << result << endl;
+	system("PAUSE");
+}
