cff8f94fac8c054eb475eb7c2862e4a804eda1bc
Kamaraju Vipin (CC-DA/EYC5) <kav8rt@bosch.com>
2020-04-08 08:53:24 +0200

    0% basic structure CountDiv.cpp

---
+++ src/CodilityLessons/05_PrefixSums/CountDiv.cpp
@@ -0,0 +1,37 @@
+
+/*
+
+Write a function:
+
+int solution(int A, int B, int K);
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+{ i : A = i = B, i mod K = 0 }
+
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Write an efficient algorithm for the following assumptions:
+
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A = B.
+
+Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+*/
+
+#include <iostream>
+
+
+int solution(int A, int B, int K)
+{
+	return A;
+}
+
+
+void main()
+{
+	system("PAUSE");
+}
+
